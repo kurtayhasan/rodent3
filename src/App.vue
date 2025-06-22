b84df3e939085cf3e96ea28419b9cfb2a96674bd
kurtayhasan <kurtayhasan@gmail.com>
2025-06-22 23:31:09 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-100 to-white">
+    <h1 class="text-4xl font-bold text-blue-800">Rodent Ağız ve Diş Sağlığı Merkezi</h1>
+    <p class="mt-4 text-lg text-gray-700">Yenişehir, Vali Ozan Blv. Aykon Plaza NO: 45 B, Artuklu/Mardin</p>
+    <p class="text-md text-gray-600">📞 0545 502 82 82 | 
+      <a class="text-blue-600 underline" href="https://www.instagram.com/rodent.agizvedismerkezi/" target="_blank">Instagram</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
