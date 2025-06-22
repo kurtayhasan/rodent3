4f8da13c2f5f0a36bc0737fd4918b7dbb17a0c09
kurtayhasan <kurtayhasan@gmail.com>
2025-06-23 01:26:17 +0300

    Add files via upload

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="bg-cover bg-center text-white py-20 px-4" style="background-image: url('/clinic-bg.jpg');">
+    <div class="text-center backdrop-blur-sm bg-black/40 py-12 px-4 rounded">
+      <h1 class="text-4xl font-bold mb-4">Rodent Ağız ve Diş Sağlığı Merkezi</h1>
+      <p class="mb-2">Yenişehir, Vali Ozan Blv. Aykon Plaza NO: 45 B, Artuklu/Mardin</p>
+      <p>📞 0545 502 82 82 | <a href="https://www.instagram.com/rodent.agizvedismerkezi/" class="underline">Instagram</a></p>
+    </div>
+  </section>
+</template>
---
+++ src/components/SliderSection.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="py-16 bg-white text-center">
+    <h2 class="text-3xl font-bold mb-6">Klinik Görselleri</h2>
+    <div class="flex overflow-x-auto space-x-4 px-4">
+      <img v-for="i in 5" :key="i" :src="'/img/slider' + i + '.jpg'" class="h-64 w-auto rounded shadow" />
+    </div>
+  </section>
+</template>
