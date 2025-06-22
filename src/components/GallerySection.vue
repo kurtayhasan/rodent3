2e5b08820f5b0eae0104bdc247f75317f20d4996
kurtayhasan <kurtayhasan@gmail.com>
2025-06-23 00:12:14 +0300

    Delete src directory

--- src/components/GallerySection.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section class="bg-white py-16 px-4 text-center">
-    <h2 class="text-3xl font-bold text-gray-800 mb-10">Klinik Galerisi</h2>
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl mx-auto">
-      <img v-for="n in 6" :key="n" :src="`/gallery/klinik${n}.jpeg`" class="rounded-lg shadow-md w-full object-cover" alt="Klinik Görseli">
-    </div>
-  </section>
-</template>
