481128be83c40c2b0f2fa48e70840536d369243a
kurtayhasan <kurtayhasan@gmail.com>
2025-06-23 00:26:54 +0300

    Add files via upload

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="bg-cover bg-center text-white py-24 px-4" style="background-image: url('/clinic-bg.jpg');">
+    <div class="text-center">
+      <h1 class="text-4xl md:text-5xl font-bold mb-4">Rodent Ağız ve Diş Sağlığı Merkezi</h1>
+      <p class="text-lg mb-6">Yenişehir, Vali Ozan Blv. Aykon Plaza NO: 45 B, Artuklu/Mardin</p>
+      <p class="text-md">📞 0545 502 82 82 | <a href="https://www.instagram.com/rodent.agizvedismerkezi/" target="_blank" class="underline">Instagram</a></p>
+    </div>
+  </section>
+</template>
