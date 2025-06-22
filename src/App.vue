395bcdc4b794222e027a68bf5acc43b48d5dc458
kurtayhasan <kurtayhasan@gmail.com>
2025-06-23 00:01:59 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-100 to-white">
-    <h1 class="text-4xl font-bold text-blue-800">Rodent Ağız ve Diş Sağlığı Merkezi</h1>
-    <p class="mt-4 text-lg text-gray-700">Yenişehir, Vali Ozan Blv. Aykon Plaza NO: 45 B, Artuklu/Mardin</p>
-    <p class="text-md text-gray-600">📞 0545 502 82 82 | 
-      <a class="text-blue-600 underline" href="https://www.instagram.com/rodent.agizvedismerkezi/" target="_blank">Instagram</a>
-    </p>
-  </div>
+  <HeroSection />
+  <ServicesSection />
+  <MissionVision />
+  <GallerySection />
+  <ContactSection />
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script setup>
+import HeroSection from './components/HeroSection.vue'
+import ServicesSection from './components/ServicesSection.vue'
+import MissionVision from './components/MissionVision.vue'
+import GallerySection from './components/GallerySection.vue'
+import ContactSection from './components/ContactSection.vue'
 </script>
