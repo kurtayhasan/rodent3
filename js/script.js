0a11df9a983a8f6675d99f941964ea0fdafbff9f
kurtayhasan <kurtayhasan@gmail.com>
2025-06-24 00:17:22 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
-// Prevent body scroll when offcanvas is open
-UIkit.util.on('#offcanvas-nav', 'beforeshow', () => {
-  document.body.style.overflow = 'hidden';
-});
-UIkit.util.on('#offcanvas-nav', 'beforehide', () => {
-  document.body.style.overflow = '';
-});
-const menuToggle = document.querySelector('.menu-toggle');
-const body = document.body;
-const navLinks = document.querySelectorAll('.nav-mobile a');
-
-menuToggle.addEventListener('click', () => {
-  body.classList.toggle('nav-open');
-});
-
-// Mobil menüdeki bir bağlantıya tıklandığında menüyü kapat
-navLinks.forEach(link => {
+// Offcanvas içindeki linke tıklayınca otomatik kapansın:
+document.querySelectorAll('#offcanvas-nav .uk-nav a').forEach(link => {
   link.addEventListener('click', () => {
-    body.classList.remove('nav-open');
+    UIkit.offcanvas('#offcanvas-nav').hide();
   });
 });
